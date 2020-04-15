e5cab2755839fcab8e614dc11c32e5e4cd3de189
Joey Ciechanowicz <joeyciechanowicz@users.noreply.github.com>
2020-04-15 12:26:55 +0100

    Remove everything

--- scripts/extract-data.js
+++ scripts/extract-data.js
@@ -1,35 +1,41 @@
-(function() {
-    /*
-    Run on:
+#!/usr/bin/env node
 
-    https://en.wikipedia.org/wiki/List_of_Greek_and_Latin_roots_in_English/A-G
-    https://en.wikipedia.org/wiki/List_of_Greek_and_Latin_roots_in_English/H-O
-    https://en.wikipedia.org/wiki/List_of_Greek_and_Latin_roots_in_English/P-Z
-     */
+const {JSDOM} = require('jsdom');
 
+async function extractData(url) {
+	// Add map in to NodeList for convenience
+	NodeList.prototype.map = function (cb) {
+		return Array.from(this).map(cb);
+	};
 
-    // Add map in to NodeList for convenience
-    NodeList.prototype.map = function(cb) {
-      return Array.from(this).map(cb);
-    }
+	const dom = await JSDOM.fromURL(url);
 
-    const rows = document.querySelectorAll('.wikitable.sortable tbody tr')
-      .map(x => x.cells)
-  .map(x => ({
-      roots: x[0],
-      meaning: x[1],
-      originLanguage: x[2],
-      examples: x[4]
-    }))
-  .filter(x => x.meaning && x.examples)
-  .map(x => ({
-      ...x,
-      meaning: x.meaning.innerText,
-      originLanguage: x.originLanguage.innerText,
-      roots: x.roots.querySelectorAll('b').map(y => y.innerText),
-    examples: x.examples.innerText.split(',').map(y => y.trim())
-  }));
+	return [...dom.document.querySelectorAll('.wikitable.sortable tbody tr')]
+		.map(x => x.cells)
+		.map(x => ({
+			roots: x[0],
+			meaning: x[1],
+			originLanguage: x[2],
+			examples: x[4]
+		}))
+		.filter(x => x.meaning && x.examples)
+		.map(x => ({
+			...x,
+			meaning: x.meaning.innerText,
+			originLanguage: x.originLanguage.innerText,
+			roots: x.roots.querySelectorAll('b').map(y => y.innerText),
+			examples: x.examples.innerText.split(',').map(y => y.trim())
+		}));
+}
 
-    copy(JSON.stringify(rows));
-  }
-)();
+async function extractAllData() {
+	const aToG = await extractData('https://en.wikipedia.org/wiki/List_of_Greek_and_Latin_roots_in_English/A-G');
+	// const hToO = await extractData('https://en.wikipedia.org/wiki/List_of_Greek_and_Latin_roots_in_English/H-O');
+	// const pToZ = await extractData('https://en.wikipedia.org/wiki/List_of_Greek_and_Latin_roots_in_English/P-Z');
+
+	return aToG;
+}
+
+extractAllData()
+	.then(console.log)
+	.catch(console.error);
