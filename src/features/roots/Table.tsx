88700da2d107808cb4938bfcaf4c131a68f7926c
Joey Ciechanowicz <joeyciechanowicz@users.noreply.github.com>
2020-06-09 13:17:15 +0100

    Adds language back in

--- src/features/roots/Table.tsx
+++ src/features/roots/Table.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import {Row} from './types';
-import {TableRow} from '../../components/Table-row';
 import {useSelector} from 'react-redux';
 import {RootState} from '../../store';
+
+const commaSeparated = (list: string[]) => list.map((item, i) => `${item}${i < list.length - 1 ? ', ' : ''}`);
+
+function TableRow({row}: {row: Row}) {
+	return (
+		<tr key={row.index}>
+			<td>{commaSeparated(row.roots)}</td>
+			<td>{row.meaning}</td>
+			<td>{row.originLanguage}</td>
+			<td>{row.etymology}</td>
+			<td>{commaSeparated(row.examples)}</td>
+		</tr>
+	);
+}
 
 export function Table() {
 	const {current} = useSelector((state: RootState) => state.roots.rows.paginated);
@@ -21,7 +34,7 @@ export function Table() {
 			<tr>
 				<th scope="col">Roots</th>
 				<th scope="col">Meaning</th>
-				{/*<th scope="col">Origin Language</th>*/}
+				<th scope="col">Origin Language</th>
 				<th scope="col">Etymology</th>
 				<th scope="col">Examples</th>
 			</tr>
